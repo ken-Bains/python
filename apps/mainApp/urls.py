343f7bff6549f4311fca5eae0c06254add9b7861
Ken Bains <kennbains@gmail.com>
2016-12-15 17:40:14 -0800

    initial

---
+++ apps/mainApp/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^new$', views.newUser),
+    url(r'^signOn$', views.signOn),
+    url(r'^success$', views.success),
+
+]
